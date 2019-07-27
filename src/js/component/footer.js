7247d1cf357a16e17d69ca42cc71b3f6896d82ac
Jorge Mair <jorgemair@yahoo.com>
2019-07-27 08:52:22 +0000

    add Revised cards

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,12 +1,14 @@
 import React from "react";
-export class footer extends React.Component {
+export class Footer extends React.Component {
 	render() {
 		return (
-            <footer className = "py-5 bg-dark">
-<div className="footer">
-   <p className ="m-0 text-center text-white" >Copyright © Your Website 2019</p>
-  </div>
-  </footer>
-        );
-    }
+			<footer className="py-5 bg-dark">
+				<div className="footer">
+					<p className="m-0 text-center text-white">
+						Copyright © Your Website 2019
+					</p>
+				</div>
+			</footer>
+		);
+	}
 }
--- src/js/home.js
+++ src/js/home.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { Navbar } from "./component/navbar.js";
 import { Jumbotron } from "./jumbotron";
-import { Card } from "./component/card.js";
-import { Card2 } from "./card2";
+import { Cards } from "./card2";
 import { Footer } from "./footer";
 //include images into your bundle
 
@@ -15,15 +14,9 @@ export class Home extends React.Component {
 				<br />
 				<Jumbotron />
 				<br />
-                <Card/>
-                <br/>
-                <Card2/>
-                <br/>
-                <Card/>
-                <br/>
-                <Card2/>
-                <br/>
-                <Footer/>
+                <Cards />
+                <br />
+                <Footer />
 			</div>
 		);
 	}
